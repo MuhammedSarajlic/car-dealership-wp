a587911816afc25e20fe8c3002d893e0fea8e969
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2022-11-16 19:49:02 +0100

    Feat: database
    -Connect to Firebase database
    -Fetch data from data base for feature section

--- client/src/Firebase/Firebase.js
+++ client/src/Firebase/Firebase.js
@@ -1,12 +1,16 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEcMHm5pmNuvoQivjDDOp9x4zv1aTP1XI",
-  authDomain: "car-dealership-2d155.firebaseapp.com",
-  projectId: "car-dealership-2d155",
-  storageBucket: "car-dealership-2d155.appspot.com",
-  messagingSenderId: "842690678953",
-  appId: "1:842690678953:web:44c4bda559fce5058319b8"
+  apiKey: "AIzaSyC4qLcFguuuUbst35krgwqf7nOaGzlkVoA",
+  authDomain: "car-dealership-3a4b3.firebaseapp.com",
+  projectId: "car-dealership-3a4b3",
+  storageBucket: "car-dealership-3a4b3.appspot.com",
+  messagingSenderId: "245861089003",
+  appId: "1:245861089003:web:6325d83801ae28776208ae",
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
--- client/src/components/FeatureSection.jsx
+++ client/src/components/FeatureSection.jsx
@@ -6,8 +6,26 @@ import {
   rightArrow,
 } from "../images/FeatureSectionImages";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../Firebase/Firebase";
 
 const FeatureSection = () => {
+  const [listOfCars, setListOfCars] = useState([]);
+
+  const carsCollectionRef = collection(db, "cars");
+
+  const getCarsList = async () => {
+    const data = await getDocs(carsCollectionRef);
+    setListOfCars(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+  useEffect(() => {
+    getCarsList();
+    console.log(listOfCars);
+    console.log(listOfCars[0]);
+  }, []);
+
   return (
     <div className="mt-82">
       <p className="text-2xl text-center text-mainColor">
@@ -16,19 +34,25 @@ const FeatureSection = () => {
       <h2 className="text-7xl text-center font-bold uppercase">Feature Car</h2>
       <div className="px-150 w-full h-453 my-100">
         <div className="bg-white w-full h-full flex">
-          <Link className="w-1/3 h-453 bg-mainColor mr-15 rounded-xl overflow-hidden">
-            <img src={f_img1} alt="feature-img" className="h-1/2 w-full" />
-            <div className="h-1/2 p-17">
-              <p className="text-white text-xl mb-10">
-                BMW M4 CS M-PERFORMANCE/ CARBON/LED/KAMERA/LEDER
-              </p>
-              <div className="w-full h-px bg-white"></div>
-              <p className="text-white text-xl my-10">Year: 2017</p>
-              <p className="text-white text-xl mb-10">Mileage: 28 km</p>
-              <div className="w-full h-px bg-white"></div>
-              <p className="text-white text-2xl mt-10">€ 121,900</p>
-            </div>
-          </Link>
+          {listOfCars.map((car) => (
+            <Link className="w-1/3 h-453 bg-mainColor mr-15 rounded-xl overflow-hidden">
+              <img
+                src={car.imageUrl}
+                alt="feature-img"
+                className="h-1/2 w-full"
+              />
+              <div className="h-1/2 p-17">
+                <p className="text-white text-xl mb-10">{car.Title}</p>
+                <div className="w-full h-px bg-white"></div>
+                <p className="text-white text-xl my-10">Year: {car.Year}</p>
+                <p className="text-white text-xl mb-10">
+                  Mileage: {car.Mileage} km
+                </p>
+                <div className="w-full h-px bg-white"></div>
+                <p className="text-white text-2xl mt-10">€ {car.Price}</p>
+              </div>
+            </Link>
+          ))}
           <Link className="w-1/3 h-full bg-mainColor mx-15 rounded-lg">
             <img src={f_img2} alt="feature-img" className="h-1/2 w-full" />
             <div className="h-1/2 p-17">
